7293fd1c87202b9524f77efac9d946547a49902e
abbybuckland1 <abby_buckland@yahoo.com>
2026-09-22 15:16:59 -0400

    Task 12- Remove redundant Card cast in Gameboard.Java, seperate variable declarations in CardsTest.java, and refactor playerMoved in GameMaster.Java to extract property buying check

--- src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameBoard.java
@@ -10,11 +10,18 @@ public class GameBoard {
 	//the key of colorGroups is the name of the color group.
 	private Hashtable<String, Integer> colorGroups = new Hashtable<String, Integer>();
 	private ArrayList<Card> communityChestCards = new ArrayList<Card>();
+	/**
+	 * Constructs a new GameBoard instance and initializes it with a 'Go' cell.
+	 */
 	public GameBoard() {
 		Cell go = new GoCell();
 		addCell(go);
 	}
 
+    /**
+     * Adds a card to the appropriate deck based on its type.
+     * @param card the card to add to the game board's decks
+     */
     public void addCard(Card card) {
         if(card.getCardType() == Card.TYPE_CC) {
             communityChestCards.add(card);
@@ -23,10 +30,18 @@ public class GameBoard {
         }
     }
 	
+	/**
+	 * Adds a generic cell to the game board's list of cells.
+	 * @param cell the cell to be added to the board
+	 */
 	public void addCell(Cell cell) {
 		cells.add(cell);
 	}
 	
+	/**
+	 * Adds a property cell to the board and updates the color group count accordingly.
+	 * @param cell the property cell to add to the board
+	 */
 	public void addCell(PropertyCell cell) {
 		String colorGroup = cell.getColorGroup();
 		int propertyNumber = getPropertyNumberForColor(colorGroup);
@@ -34,13 +49,21 @@ public class GameBoard {
         cells.add(cell);
 	}
 
+    /**
+     * Draws the top Community Chest card, removes it from the deck, then re-adds it to the bottom.
+     * @return the Community Chest card drawn
+     */
     public Card drawCCCard() {
-        Card card = (Card)communityChestCards.get(0);
+        Card card = communityChestCards.get(0);
         communityChestCards.remove(0);
         addCard(card);
         return card;
     }
 
+    /**
+     * Draws the top Chance card, removes it from the deck, then re-adds it to the bottom.
+     * @return the Chance card drawn
+     */
     public Card drawChanceCard() {
         Card card = (Card)chanceCards.get(0);
         chanceCards.remove(0);
@@ -48,14 +71,28 @@ public class GameBoard {
         return card;
     }
 
+	/**
+	 * Retrieves the cell at the specified index on the game board.
+	 * @param newIndex the index of the cell to retrieve
+	 * @return the cell at the given index
+	 */
 	public Cell getCell(int newIndex) {
 		return (Cell)cells.get(newIndex);
 	}
 	
+	/**
+	 * Returns the total number of cells on the game board.
+	 * @return the number of cells
+	 */
 	public int getCellNumber() {
 		return cells.size();
 	}
 	
+	/**
+	 * Returns an array of property cells belonging to the specified color group (monopoly).
+	 * @param color the color group name to retrieve properties for
+	 * @return an array of property cells in the specified color group
+	 */
 	public PropertyCell[] getPropertiesInMonopoly(String color) {
 		PropertyCell[] monopolyCells = 
 			new PropertyCell[getPropertyNumberForColor(color)];
@@ -73,6 +110,11 @@ public class GameBoard {
 		return monopolyCells;
 	}
 	
+	/**
+	 * Returns the number of properties for a specified color group.
+	 * @param name the name of the color group
+	 * @return the count of properties in that color group
+	 */
 	public int getPropertyNumberForColor(String name) {
 		Integer number = (Integer)colorGroups.get(name);
 		if(number != null) {
@@ -81,6 +123,11 @@ public class GameBoard {
 		return 0;
 	}
 
+	/**
+	 * Finds and returns the cell with the specified name.
+	 * @param string the name of the cell to search for
+	 * @return the cell with the matching name or null if not found
+	 */
 	public Cell queryCell(String string) {
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -91,6 +138,11 @@ public class GameBoard {
 		return null;
 	}
 	
+	/**
+	 * Finds and returns the index of the cell with the specified name.
+	 * @param string the name of the cell to search for
+	 * @return the index of the matching cell or -1 if not found
+	 */
 	public int queryCellIndex(String string){
 		for(int i = 0; i < cells.size(); i++){
 			Cell temp = (Cell)cells.get(i); 
@@ -101,6 +153,9 @@ public class GameBoard {
 		return -1;
 	}
 
+    /**
+     * Clears all Community Chest cards from the deck.
+     */
     public void removeCards() {
         communityChestCards.clear();
     }
--- src/edu/towson/cis/cosc442/project1/monopoly/GameMaster.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/GameMaster.java
@@ -17,6 +17,10 @@ public class GameMaster {
 	private int utilDiceRoll;
 	private boolean testMode;
 
+	/**
+	 * Returns the singleton instance of the GameMaster class.
+	 * @return The singleton GameMaster instance.
+	 */
 	public static GameMaster instance() {
 		if(gameMaster == null) {
 			gameMaster = new GameMaster();
@@ -24,15 +28,25 @@ public class GameMaster {
 		return gameMaster;
 	}
 
+	/**
+	 * Constructs a GameMaster object initializing the starting amount of money and dice.
+	 */
 	public GameMaster() {
 		initAmountOfMoney = 1500;
 		dice = new Die[]{new Die(), new Die()};
 	}
 
+    /**
+     * Handles the event when the Buy House button is clicked by showing the buy house dialog for the current player.
+     */
     public void btnBuyHouseClicked() {
         gui.showBuyHouseDialog(getCurrentPlayer());
     }
 
+    /**
+     * Processes drawing a Community Chest or Chance card depending on the current position and applies its action.
+     * @return The drawn Card object.
+     */
     public Card btnDrawCardClicked() {
         gui.setDrawCardEnabled(false);
         CardCell cell = (CardCell)getCurrentPlayer().getPosition();
@@ -48,6 +62,9 @@ public class GameMaster {
         return card;
     }
 
+    /**
+     * Handles the end of the current player's turn, disabling/enabling appropriate buttons and updating the GUI accordingly.
+     */
     public void btnEndTurnClicked() {
 		setAllButtonEnabled(false);
 		getCurrentPlayer().getPosition().playAction();
@@ -67,6 +84,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Handles the get out of jail event for the current player and updates button states based on bankrupt or jail status.
+     */
     public void btnGetOutOfJailClicked() {
 		getCurrentPlayer().getOutOfJail();
 		if(getCurrentPlayer().isBankrupt()) {
@@ -85,6 +105,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Processes the property purchase action for the current player and updates the GUI.
+     */
     public void btnPurchasePropertyClicked() {
         Player player = getCurrentPlayer();
 		player.purchase();
@@ -92,6 +115,9 @@ public class GameMaster {
 		updateGUI();
     }
     
+    /**
+     * Rolls the dice, shows the roll result, moves the current player, and updates corresponding buttons.
+     */
     public void btnRollDiceClicked() {
 		int[] rolls = rollDice();
 		if((rolls[0]+rolls[1]) > 0) {
@@ -109,6 +135,9 @@ public class GameMaster {
 		}
     }
 
+    /**
+     * Opens the trade dialog, processes the trade deal, and updates the GUI upon successful trade completion.
+     */
     public void btnTradeClicked() {
         TradeDialog dialog = gui.openTradeDialog();
         TradeDeal deal = dialog.getTradeDeal();
@@ -121,6 +150,10 @@ public class GameMaster {
         }
     }
 
+    /**
+     * Completes the trade between the current player and the specified seller according to the trade deal details.
+     * @param deal The TradeDeal object containing trade details.
+     */
     public void completeTrade(TradeDeal deal) {
         Player seller = getPlayer(deal.getPlayerIndex());
         Cell property = gameBoard.queryCell(deal.getPropertyName());
@@ -128,51 +161,101 @@ public class GameMaster {
         getCurrentPlayer().buyProperty(property, deal.getAmount());
     }
 
+    /**
+     * Draws and returns a Community Chest card from the game board.
+     * @return The drawn Community Chest Card.
+     */
     public Card drawCCCard() {
         return gameBoard.drawCCCard();
     }
 
+    /**
+     * Draws and returns a Chance card from the game board.
+     * @return The drawn Chance Card.
+     */
     public Card drawChanceCard() {
         return gameBoard.drawChanceCard();
     }
 
 	
+	/**
+	 * Returns the player whose turn it currently is.
+	 * @return The current Player object.
+	 */
 	public Player getCurrentPlayer() {
 		return getPlayer(turn);
 	}
     
+    /**
+     * Returns the index of the current player whose turn it is.
+     * @return Integer index of the current player.
+     */
     public int getCurrentPlayerIndex() {
         return turn;
     }
 
+	/**
+	 * Returns the game board being used in the game.
+	 * @return The GameBoard instance.
+	 */
 	public GameBoard getGameBoard() {
 		return gameBoard;
 	}
 
+    /**
+     * Returns the GUI instance managing the game interface.
+     * @return The MonopolyGUI instance.
+     */
     public MonopolyGUI getGUI() {
         return gui;
     }
 
+	/**
+	 * Returns the initial amount of money assigned to players at the start of the game.
+	 * @return The initial starting money amount.
+	 */
 	public int getInitAmountOfMoney() {
 		return initAmountOfMoney;
 	}
 	
+	/**
+	 * Returns the current number of players in the game.
+	 * @return The number of players.
+	 */
 	public int getNumberOfPlayers() {
 		return players.size();
 	}
 
+    /**
+     * Returns the number of possible sellers excluding the current player.
+     * @return The count of other players available as sellers.
+     */
     public int getNumberOfSellers() {
         return players.size() - 1;
     }
 
+	/**
+	 * Returns the Player object at the specified index.
+	 * @param index The index of the player to retrieve.
+	 * @return The Player at the given index.
+	 */
 	public Player getPlayer(int index) {
 		return (Player)players.get(index);
 	}
 	
+	/**
+	 * Returns the index of the specified player in the players list.
+	 * @param player The Player object to find.
+	 * @return The index of the player, or -1 if not found.
+	 */
 	public int getPlayerIndex(Player player) {
 		return players.indexOf(player);
 	}
 
+    /**
+     * Returns a list of players excluding the current player for use as possible sellers in trades.
+     * @return ArrayList of Player objects excluding the current player.
+     */
     public ArrayList<Player> getSellerList() {
         ArrayList<Player> sellers = new ArrayList<Player>();
         for (Iterator<Player> iter = players.iterator(); iter.hasNext();) {
@@ -182,19 +265,37 @@ public class GameMaster {
         return sellers;
     }
 
+	/**
+	 * Returns the index representing the current turn's player.
+	 * @return The current turn index.
+	 */
 	public int getTurn() {
 		return turn;
 	}
 
+	/**
+	 * Returns the utility dice roll value stored in the GameMaster.
+	 * @return The utility dice roll integer value.
+	 */
 	public int getUtilDiceRoll() {
 		return this.utilDiceRoll;
 	}
 
+	/**
+	 * Moves the player at the given index forward by the specified dice value, updating position and GUI.
+	 * @param playerIndex The index of the player to move.
+	 * @param diceValue The number of spaces to move the player.
+	 */
 	public void movePlayer(int playerIndex, int diceValue) {
 		Player player = (Player)players.get(playerIndex);
 		movePlayer(player, diceValue);
 	}
 	
+	/**
+	 * Moves the specified player forward by the dice value, handles passing Go, updates position and GUI.
+	 * @param player The Player to move.
+	 * @param diceValue The number of spaces to advance the player.
+	 */
 	public void movePlayer(Player player, int diceValue) {
 		Cell currentPosition = player.getPosition();
 		int positionIndex = gameBoard.queryCellIndex(currentPosition.getName());
@@ -208,23 +309,40 @@ public class GameMaster {
 		updateGUI();
 	}
 
+	/**
+	 * Updates the GUI controls based on the player's new position after moving.
+	 * @param player The Player who just moved.
+	 */
 	public void playerMoved(Player player) {
 		Cell cell = player.getPosition();
 		int playerIndex = getPlayerIndex(player);
 		if(cell instanceof CardCell) {
 		    gui.setDrawCardEnabled(true);
 		} else{
-			if(cell.isAvailable()) {
-				int price = cell.getPrice();
-				if(price <= player.getMoney() && price > 0) {
-					gui.enablePurchaseBtn(playerIndex);
-				}
-			}	
+			checkPropertyAvailableAndEnoughMoney(player, cell, playerIndex);	
 			gui.enableEndTurnBtn(playerIndex);
 		}
         gui.setTradeEnabled(turn, false);
 	}
 
+	/**
+	 * Checks if a property is available and if the player has enough money to purchase it, enabling purchase button accordingly.
+	 * @param player The Player attempting to purchase.
+	 * @param cell The Cell property to check.
+	 * @param playerIndex The index of the player for GUI enabling.
+	 */
+	private void checkPropertyAvailableAndEnoughMoney(Player player, Cell cell, int playerIndex) {
+		if(cell.isAvailable()) {
+			int price = cell.getPrice();
+			if(price <= player.getMoney() && price > 0) {
+				gui.enablePurchaseBtn(playerIndex);
+			}
+		}
+	}
+
+	/**
+	 * Resets the game state by moving all players to the starting position and removing cards from the board.
+	 */
 	public void reset() {
 		for(int i = 0; i < getNumberOfPlayers(); i++){
 			Player player = (Player)players.get(i);
@@ -234,6 +352,10 @@ public class GameMaster {
 		turn = 0;
 	}
 	
+	/**
+	 * Rolls the dice to generate dice values, using test mode values if enabled.
+	 * @return An int array containing two dice roll results.
+	 */
 	public int[] rollDice() {
 		if(testMode) {
 			return gui.getDiceRoll();
@@ -246,6 +368,10 @@ public class GameMaster {
 		}
 	}
 	
+	/**
+	 * Sends the specified player to the Jail position, marks them as in jail, and updates the GUI.
+	 * @param player The Player to be sent to Jail.
+	 */
 	public void sendToJail(Player player) {
 	    int oldPosition = gameBoard.queryCellIndex(getCurrentPlayer().getPosition().getName());
 		player.setPosition(gameBoard.queryCell("Jail"));
@@ -257,6 +383,10 @@ public class GameMaster {
 		        jailIndex);
 	}
     
+	/**
+	 * Enables or disables all main game action buttons in the GUI based on the given flag.
+	 * @param enabled True to enable all buttons; false to disable.
+	 */
 	private void setAllButtonEnabled(boolean enabled) {
 		gui.setRollDiceEnabled(enabled);
 		gui.setPurchasePropertyEnabled(enabled);
@@ -267,18 +397,34 @@ public class GameMaster {
         gui.setGetOutOfJailEnabled(enabled);
 	}
 
+	/**
+	 * Sets the game board to be used for this game session.
+	 * @param board The GameBoard object to set.
+	 */
 	public void setGameBoard(GameBoard board) {
 		this.gameBoard = board;
 	}
 	
+	/**
+	 * Sets the GUI interface for the game.
+	 * @param gui The MonopolyGUI instance to set.
+	 */
 	public void setGUI(MonopolyGUI gui) {
 		this.gui = gui;
 	}
 
+	/**
+	 * Sets the initial amount of money given to each player at the start of the game.
+	 * @param money The amount of money to assign at game start.
+	 */
 	public void setInitAmountOfMoney(int money) {
 		this.initAmountOfMoney = money;
 	}
 
+	/**
+	 * Sets the number of players and initializes each player with the starting money.
+	 * @param number The total number of players to initialize.
+	 */
 	public void setNumberOfPlayers(int number) {
 		players.clear();
 		for(int i =0;i<number;i++) {
@@ -288,16 +434,26 @@ public class GameMaster {
 		}
 	}
 
+	/**
+	 * Stores a utility dice roll value for use in the game.
+	 * @param diceRoll The dice roll value to store.
+	 */
 	public void setUtilDiceRoll(int diceRoll) {
 		this.utilDiceRoll = diceRoll;
 	}
 	
+	/**
+	 * Starts the game by triggering the GUI to initiate and enable the first player's turn.
+	 */
 	public void startGame() {
 		gui.startGame();
 		gui.enablePlayerTurn(0);
         gui.setTradeEnabled(0, true);
 	}
 
+	/**
+	 * Moves to the next player's turn, updating GUI controls based on jail status and player capabilities.
+	 */
 	public void switchTurn() {
 		turn = (turn + 1) % getNumberOfPlayers();
 		if(!getCurrentPlayer().isInJail()) {
@@ -310,14 +466,24 @@ public class GameMaster {
 		}
 	}
 	
+	/**
+	 * Updates the GUI to reflect the current state of the game.
+	 */
 	public void updateGUI() {
 		gui.update();
 	}
 
+	/**
+	 * Displays a utility dice roll dialog and stores the result in the utility dice roll field.
+	 */
 	public void utilRollDice() {
 		this.utilDiceRoll = gui.showUtilDiceRoll();
 	}
 
+	/**
+	 * Sets whether the game is in test mode, affecting dice roll behavior.
+	 * @param b True to enable test mode, false to disable.
+	 */
 	public void setTestMode(boolean b) {
 		testMode = b;
 	}
